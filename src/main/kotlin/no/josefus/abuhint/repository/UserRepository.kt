e912eaf3ccfb53e27b8640f61ed95f52f0746828
Tofik Sahraoui <tofiksa@gmail.com>
2025-04-13 11:07:31 +0200

    fix: refaktorering, fjerner alt jeg ikke trenger for langchain

--- src/main/kotlin/no/josefus/abuhint/repository/UserRepository.kt
+++
@@ -1,6 +0,0 @@
-package no.josefus.abuhint.repository
-
-import no.josefus.abuhint.model.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long>
